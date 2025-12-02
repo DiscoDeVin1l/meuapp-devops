a3d656d822b9eccfdecd71dfaac1c89e7a607bc2
Vinicius <vinisilvam31@gmail.com>
2025-12-02 12:41:09 -0300

    Primeiro commit - App Flask + Docker + NGINX + MySQL

---
+++ app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Olááá! Minha aplicação Flask está rodando em Docker + NGINX!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
